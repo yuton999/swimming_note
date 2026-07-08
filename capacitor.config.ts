2226d5e6021b3cc40feb77e1d7f32fd9f1bde27e
yuton999 <yuton999@outlook.jp>
2026-07-08 22:36:23 +0900

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.yuton999.swimmingnote',
-  appName: 'my swimming note',
-  webDir: 'www'
-};
-
-export default config;
