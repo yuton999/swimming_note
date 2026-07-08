d0457a5b4f21f121f2d20f53ab9691a0dba91c3d
yuton999 <yuton999@outlook.jp>
2026-07-08 22:05:39 +0900

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yuton999.swimmingnote',
+  appName: 'my swimming note',
+  webDir: 'www'
+};
+
+export default config;
